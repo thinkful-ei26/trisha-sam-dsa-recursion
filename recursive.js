b976c2fe585c6cc88135cac826d5b3d9653d7c07
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-14 16:40:51 -0500

    anagrams

--- recursive.js
+++ recursive.js
@@ -101,31 +101,126 @@ var res = str.split(' ');
 console.log(res); // [ 'How', 'are', 'you', 'doing', 'today?' ] */
 
 // const split = (str, separator) => {
-//   //base case
-//   if(!str.length){
-//     return []; 
+//   // base case
+//   const location = str.indexOf(separator);
+//   console.log(location);
+//   if(location < 0){
+//     return [str];
 //   }
-//   if (separator(str[0])) {
-//     return [str[0], ...split(str.slice(1), separator)]; 
-//   }
-//   //recursive case
-//   return split(str.slice(1), separator);
+
+//   // take out the first word and then also split(str, separator)
+//   let leftovers = str.slice(location + 1) ;
+//   //console.log(leftovers);
+//   return [str.slice(0, location),  ...split(leftovers, separator)];
 // };
 
-// const _str = 'hello  asdfasdfasdfas llasdfalasdkhjlsdl'; 
-// console.log(split(_str, char => char === ' '));
+// const _str = 'hello its me'; 
+// console.log(split(_str, ' '));
 
-const split = (str, separator) => {
-  //base case
-  if(!str.length){
-    return []; 
+/* Binary Representation */ 
+
+// input: num
+// output binary representation
+// What is the input to each recursive calls = num/2
+// What is the output of each recursive calls = num%2
+// 
+
+// const binary = (num) => {
+//   if (num === 0){
+//     return '';
+//   }
+//   return binary(Math.floor(num/2)) + (num%2);
+// };
+
+// console.log(binary(38));
+
+/* === FACTORIAL === */
+
+// input: num
+// output: factorial of num
+// What is the input to each recursive calls = num -1
+// What is the output of each recursive calls = num * num -1
+
+// const factorial = (num) => {
+//   if(num === 1){
+//     return num;
+//   }
+//   // console.log(num);
+//   return num * factorial(num-1);
+// };
+
+// console.log(factorial(5));
+
+/* === FIBONNACI === */
+
+// input: num
+// output: fibonacci of at num
+// What is the input to each recursive calls = num + 1
+// What is the output of each recursive calls = num + fib(num +1)
+// const fibonacci = (num) => {
+//   let array = [0]
+//    if (array.length === num ){
+//      return array[num]
+
+//    }
+   
+//    for (let i = 1; i <num; i++) {
+//      array.push(array[i] + array[i+1])
+//    }
+//   //  console.log(array);
+//   //  return [...array, 0, 1 ] fibonacci([...array 1]) 
+//  }
+
+//  0 1 1 2 3 5
+// fibonacci(n -1) + fibonacci(n -2) 
+// console.log(fibonacci(7));
+
+
+// const fib = (num) => {
+//   //base case
+//   if(num <= 1){
+//     return num;
+//   }
+//   //recursive
+//   return fib(num -1) + fib(num -2);
+// };
+
+// console.log(fib(3));
+
+/* ===== Anagrams ===== */
+// input: string 'east'
+// output: 24 permutations 
+// recursive input: 'ast' rest of string
+// recursive output: string.slice(1) + 'ast'
+
+const anagrams = (str) => {
+  const array = [];
+  if(str.length === 1){
+    array.push(str);
+    // return str[0];
+    return array;
   }
-  if (str[0] === separator) {
-    return [str[0], ...split(str.slice(1), separator)]; 
-  }
-  //recursive case
-  return str[0] + split(str.slice(1), separator);
+  
+  // console.log(str.split(''));
+  const prefix = str[0];
+  const substring = str.slice(1);
+  console.log(substring);
+
+  // ast, sat, sta, tas, tsa, ats
+  // split into characters and rearrage them [e,a,s,t]
+  // index of each character const unchanging = [e, a, s, t];
+  // unchanging[0] + unchanging[2] + unchanging[1] + unchanging[3]
+
+  // for(let i=0; i<substring.length; i++){
+  //   const something = substring[i] + substring[i +1];
+  //   console.log(something);
+  // }
+  
+  // console.log(substring);
+  // console.log(prefix);
+  //substring needs to be different each recursion
+  array.push(anagrams(substring) + prefix);
+  return array;
 };
 
-const _str = 'hello  asdfasdfasdfas llasdfalasdkhjlsdl'; 
-console.log(split(_str, 'l'));
+console.log(anagrams('east'));
