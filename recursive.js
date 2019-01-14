0248cb047390605c5e4c0cbcc64a2601a2418554
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-14 11:46:25 -0500

    added array double

--- recursive.js
+++ recursive.js
@@ -25,3 +25,24 @@ const countSheep = (num) => {
 };
 
 countSheep(3);
+
+// Array Double
+
+// What is the input to the program = [numbers] 
+// What is the output of the program =  [2xnumbers] 
+
+// What is the input to each recursive calls = 2num
+// What is the output of each recursive calls ...nums
+
+const arrayDubs = (array) => {
+  //base case
+  if (array.length === 0) {
+    return [];
+  }
+  //  const newArray = 2 * array[0];
+  const newArray = [2 * array[0], ...arrayDubs(array.slice(1)) ];
+  //  console.log(newArray);
+  return newArray;
+};
+
+console.log(arrayDubs([1, 2, 3]));
